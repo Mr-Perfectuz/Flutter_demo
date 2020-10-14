27aa8db6b45276dbca28d328ed2c43d1cac6ce53
Mr.Perfect <ziyodovanvar1999@gmail.com>
2020-10-14 19:15:41 +0500

    widgets have been updated

--- lib/main.dart
+++ lib/main.dart
@@ -7,12 +7,27 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favorite color ?',
+      'what\'s your favorite animal ? ',
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My first app'),
         ),
-        body: Text('This is my default text !'),
+        body: Column(
+          children: [
+            Text('The question !'),
+            RaisedButton(
+              child: Text('Answer 1'),
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+            )
+            RaisedButton(child: Text('Answer 3'),),
+          ],
+        ),
       ),
     );
   }
